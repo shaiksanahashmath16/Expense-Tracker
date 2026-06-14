25dd7d1f77565f65b0e9b043932ff709a1942328
shaiksanahashmath16 <shaiksanahashmathksssz@gmail.com>
2026-06-14 11:54:09 +0530

    Implement expense tracking functionality

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 expenses = []
 print("Welcome to Expense Tracker")
+while True:
+    print("\nExpense Tracker")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Show Total")
+    print("4. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        name = input("Enter expense name: ")
+        amount = float(input("Enter amount: "))
+        expenses.append([name, amount])
+        print("Expense added successfully!")
+    elif choice == "2":
+        print("\nExpenses:")
+        for expense in expenses:
+            print(expense[0], "-", expense[1])
+    elif choice == "3":
+        total = 0
+        for expense in expenses:
+            total += expense[1]
+        print("Total Spending:", total)
+    elif choice == "4":
+        print("Thank you!")
+        break
+
+    else:
+        print("Invalid choice")
